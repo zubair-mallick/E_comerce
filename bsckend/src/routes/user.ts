9388c7c2dbdcd8ac345cda67eb49b419173f6b5f
zubair mallik <https://github.com/zubair-mallick/E_comerce>
2024-10-17 01:34:48 +0530

    add isAdminmiddleware

--- bsckend/src/routes/user.ts
+++ bsckend/src/routes/user.ts
@@ -1,12 +1,13 @@
 import  express  from "express";
 import { deleteUser, getAllUsers, getUser, newUser } from "../controllers/user.js";
+import { isAdmin } from "../middleware/auth.js";
 
 const app= express.Router();
 
 app.post("/new", newUser);
 
-app.get("/all", getAllUsers);
-app.route("/:id").get(getUser).delete(deleteUser)
+app.get("/all",isAdmin, getAllUsers);
+app.route("/:id").get(getUser).delete(isAdmin,deleteUser)
 
 
 
