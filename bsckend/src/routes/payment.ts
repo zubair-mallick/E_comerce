f5632c20b7c3ea68a4edb42198459c2020b8a615
zubair mallik <https://github.com/zubair-mallick/E_comerce>
2024-10-30 18:27:13 +0530

    apply discount controller updated

--- bsckend/src/routes/payment.ts
+++ bsckend/src/routes/payment.ts
@@ -1,11 +1,12 @@
-import  express  from "express";
-import { isAdmin } from "../middleware/auth.js";
-import { newCoupon } from "../controllers/payment.js";
+import express from "express";
+import { applyDiscount, newCoupon } from "../controllers/payment.js";
 
 
 const app= express.Router();
 
 app.post("/coupon/new",newCoupon)
+app.post("/discount",applyDiscount)
+
 
 
 
